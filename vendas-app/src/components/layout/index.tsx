0fdb9888a3b1427384c3c365ccfa1af4054f9255
Anderson Daniel <anderbreiksondaniel@gmail.com>
2023-03-20 16:05:03 -0300

    primeiro commit do arquivo

---
+++ vendas-app/src/components/layout/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Layout: React.FC = () => {
+    return (
+        <div className="app">
+            <section className="main-contain collum is-fullheight">
+
+            </section>
+        </div>
+    )
+}
